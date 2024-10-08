d04b525805d32aa9c4cc18392aa731e6faf3599a
aldu1n <cnndo14@gmail.com>
2024-10-08 12:44:50 -0400

    navbar and sidebar added, reformated other files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Projects</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <div id="sidebar">
+        <ul>
+          <li>
+            <a href="">
+              <i class="bi bi-house-fill"></i>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <i class="bi bi-file-person-fill"></i>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <i class="bi bi-briefcase-fill"></i>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <i class="bi bi-envelope-fill"></i>
+            </a>
+          </li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id="hero">
+        <div>
+          <h1>Hello, my name is Slim Shady</h1>
+          <p>Front End Developer</p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
